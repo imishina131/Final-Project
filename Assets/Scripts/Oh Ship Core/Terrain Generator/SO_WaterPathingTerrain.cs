a8d81fbe8d2cf7947a1e45e77ceba1497affe143
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2026-05-25 14:58:24 -0400

    Added Test scene and script
    I did a thing :3

---
+++ Assets/Scripts/Oh Ship Core/Terrain Generator/SO_WaterPathingTerrain.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WaterPathData", menuName = "Scriptable Objects/Terrain")]
+public class SO_WaterPathingTerrain : ScriptableObject
+{
+    public GameObject[] waterPaths;
+}
