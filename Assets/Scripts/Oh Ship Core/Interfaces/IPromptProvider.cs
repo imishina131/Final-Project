6ab8bcc3d8577ff39825cca47630cf8ed43921e1
Colin Whiteford <colinwhiteford516@gmail.com>
2026-06-08 15:20:06 -0400

    Prepped Prompt Display Setup

--- Assets/Scripts/Oh Ship Core/Interfaces/IPromptProvider.cs
+++ Assets/Scripts/Oh Ship Core/Interfaces/IPromptProvider.cs
@@ -1,10 +1,13 @@
 using System;
+using JetBrains.Annotations;
+using UnityEngine;
 
 /// <summary>
 /// Represents an object that can display a prompt to the user. Used for things like notifying the player that an object is interactable.
 /// </summary>
 public interface IPromptProvider
 {
-    public void ProvidePrompt(PromptData promptData);
-    public string GetRequestedWidgetName();
+    [Pure] public PromptData GetPromptData();
+    [Pure] public string GetRequestedWidgetName();
+    [Pure] public Vector3 GetRequestedWorldPosition();
 }
--- Assets/Scripts/Oh Ship Core/UI/PromptDisplay.cs
+++ Assets/Scripts/Oh Ship Core/UI/PromptDisplay.cs
@@ -1,16 +1,50 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class PromptDisplay : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Camera m_connectedCamera;
+    [SerializeField] RectTransform m_promptContainer;
+    [SerializeField] SerializableDictionary<string, GameObject> m_promptPrefabs = new();
+    readonly Dictionary<IPromptProvider, RectTransform> m_activePrompts = new();
+    readonly Dictionary<string, ObjectPool<RectTransform>> m_pools = new();
+
     void Start()
     {
-        
+        foreach (KeyValuePair<string, GameObject> promptPrefab in m_promptPrefabs)
+        {
+            m_pools.Add(promptPrefab.Key, new(
+                () => Instantiate(promptPrefab.Value).GetComponent<RectTransform>(),
+                rectTransform => rectTransform.gameObject.SetActive(true), 
+                rectTransform => rectTransform.gameObject.SetActive(false)
+            ));
+        }
+    }
+    public void AddPrompt(IPromptProvider promptProvider)
+    {
+        if (m_activePrompts.ContainsKey(promptProvider)) return;
+        if(!m_pools.TryGetValue(promptProvider.GetRequestedWidgetName(), out ObjectPool<RectTransform> pool)) return;
+        RectTransform promptRect = pool.Get();
+        promptProvider.GetPromptData().Apply(promptRect.gameObject);
+        m_activePrompts.Add(promptProvider, promptRect);
     }
 
-    // Update is called once per frame
+    public void RemovePrompt(IPromptProvider promptProvider)
+    {
+        if (!m_activePrompts.Remove(promptProvider, out RectTransform promptRect)) return;
+        if (m_pools.TryGetValue(promptProvider.GetRequestedWidgetName(), out ObjectPool<RectTransform> pool)) pool.Release(promptRect);
+    }
+
     void Update()
     {
-        
+        if (m_activePrompts.Count <= 0) return;
+        foreach (KeyValuePair<IPromptProvider, RectTransform> prompt in m_activePrompts)
+        {
+            Vector3 screenPos = m_connectedCamera.WorldToScreenPoint(prompt.Key.GetRequestedWorldPosition());
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(m_promptContainer, screenPos, m_connectedCamera, out Vector2 localPoint);
+            prompt.Value.anchoredPosition = localPoint;
+        }
     }
+
 }
