02324680bbbd9c0066913fbe2268a437bfd9ed81
Colin Whiteford <colinwhiteford516@gmail.com>
2026-06-19 13:38:12 -0400

    Cooking Update

--- Assets/Scripts/Matrix Utils/VFX/CalculateObjectVelocity.cs
+++ Assets/Scripts/Matrix Utils/VFX/CalculateObjectVelocity.cs
@@ -4,11 +4,10 @@ using UnityEngine.VFX;
 
 public class CalculateObjectVelocity : VFXSpawnerCallbacks
 {
-    static readonly int s_PositionPropertyId = Shader.PropertyToID("ObjectPositionWS");
-
-    static readonly int s_PositionAttributeId = Shader.PropertyToID("position");
-    static readonly int s_OldPositionAttributeId = Shader.PropertyToID("oldPosition");
-    static readonly int s_VelocityAttributeId = Shader.PropertyToID("velocity");
+    static readonly int s_positionPropertyId = Shader.PropertyToID("ObjectPositionWS");
+    static readonly int s_positionAttributeId = Shader.PropertyToID("position");
+    static readonly int s_oldPositionAttributeId = Shader.PropertyToID("oldPosition");
+    static readonly int s_velocityAttributeId = Shader.PropertyToID("velocity");
 
     public class InputProperties
     {
@@ -18,19 +17,19 @@ public class CalculateObjectVelocity : VFXSpawnerCallbacks
 
     public override void OnPlay(VFXSpawnerState state, VFXExpressionValues vfxValues, VisualEffect vfxComponent)
     {
-        m_lastPosition = vfxValues.GetVector3(s_PositionPropertyId);
+        m_lastPosition = vfxValues.GetVector3(s_positionPropertyId);
     }
 
     public override void OnUpdate(VFXSpawnerState state, VFXExpressionValues vfxValues, VisualEffect vfxComponent)
     {
         if (!state.playing || state.deltaTime == 0) return;
 
-        float3 position = vfxValues.GetVector3(s_PositionPropertyId);
+        float3 position = vfxValues.GetVector3(s_positionPropertyId);
 
-        state.vfxEventAttribute.SetVector3(s_OldPositionAttributeId, m_lastPosition);
-        state.vfxEventAttribute.SetVector3(s_PositionAttributeId, position);
+        state.vfxEventAttribute.SetVector3(s_oldPositionAttributeId, m_lastPosition);
+        state.vfxEventAttribute.SetVector3(s_positionAttributeId, position);
 
-        state.vfxEventAttribute.SetVector3(s_VelocityAttributeId, (position - m_lastPosition) / state.deltaTime);
+        state.vfxEventAttribute.SetVector3(s_velocityAttributeId, (position - m_lastPosition) / state.deltaTime);
 
         m_lastPosition = position;
     }
