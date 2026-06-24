d09f64a44c1b1dba18cf26646a38c77436b3cad3
Irina Mishina <irinamishina131@gmail.com>
2026-06-24 13:13:30 -0400

    crab cooking

---
+++ Assets/Scripts/Oh Ship Core/Fish/Food/Crab.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Crab : FoodClass
+{
+    private Material m_material;
+    private float m_cookedAmount;
+    private CookState m_currentCookState = CookState.Raw;
+
+    public override CookState CookStateRef { get { return m_currentCookState; } }
+
+    public override CookingProcess CookingProcess => CookingProcess.InPot;
+
+    private void Start()
+    {
+        m_material = GetComponent<MeshRenderer>().material;
+    }
+
+    public override void UpdateCookedAmount(float incomingAmount)
+    {
+        m_cookedAmount = incomingAmount;
+        m_material.SetFloat("_Cooked_Amount", m_cookedAmount);
+
+        CookState newState = DetermineCookState(m_cookedAmount);
+
+        if (newState == m_currentCookState) return;
+        m_currentCookState = newState;
+    }
+
+    public override float Eat()
+    {
+        return foodData.HungerRestored(m_currentCookState);
+    }
+
+
+    CookState DetermineCookState(float cookedAmount)
+    {
+        if (foodData.GetThreshold(CookState.Burnt) >= 0 && cookedAmount >= foodData.GetThreshold(CookState.Burnt))
+            return CookState.Burnt;
+
+        if (foodData.GetThreshold(CookState.Cooked) >= 0 && cookedAmount >= foodData.GetThreshold(CookState.Cooked))
+            return CookState.Cooked;
+
+        return CookState.Raw;
+    }
+}
