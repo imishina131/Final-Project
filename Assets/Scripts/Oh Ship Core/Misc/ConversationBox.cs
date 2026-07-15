f18c725313a82583ce7910db0ed7cc5c15ab408e
Irina Mishina <irinamishina131@gmail.com>
2026-07-15 14:00:02 -0400

    convo

--- Assets/Scripts/Oh Ship Core/Misc/ConversationBox.cs
+++ Assets/Scripts/Oh Ship Core/Misc/ConversationBox.cs
@@ -7,6 +7,16 @@ public class ConversationBox : MonoBehaviour
     [SerializeField] AudioClip m_enterSound;
     [Space]
     [SerializeField] AudioClip m_exitSound;
+    [Space]
+    [SerializeField] AudioClip beginningSound;
+    [Space]
+    [SerializeField] AudioClip randomSound01;
+    [Space]
+    [SerializeField] AudioClip randomSound02;
+    [Space]
+    [SerializeField] AudioClip randomSound03;
+    [Space]
+    [SerializeField] AudioClip randomSound04;
     private AudioSource m_audioSource;
     private string playerTag = "Player";
     private bool passed = false;
@@ -23,6 +33,24 @@ public class ConversationBox : MonoBehaviour
     {
         if (other.CompareTag(playerTag) && !passed)
         {
+            switch(other.tag)
+            {
+                case "Beginning":
+                    m_enterSound = beginningSound;
+                    break;
+                case "Voiceline01":
+                    m_enterSound = randomSound01;
+                    break;
+                case "Voiceline02":
+                    m_enterSound = randomSound02;
+                    break;
+                case "Voiceline03":
+                    m_enterSound = randomSound03;
+                    break;
+                case "Voiceline04":
+                    m_enterSound = randomSound04;
+                    break;
+            }
             StartCoroutine(PlayAudio());
             passed = true;
         }
